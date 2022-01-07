328d58f62df2ed5e7f8bdbbe6b87295dc854084a
Nathaniel Johnson <nsj806@gmail.com>
2022-01-07 01:54:29 -0500

    Fix floats for big numbers (#13)
    * fix floats for big numbers
    * added tests
    
    See issue: #12

--- numerizer/numerizer.py
+++ numerizer/numerizer.py
@@ -201,13 +201,17 @@ def numerize_big_prefixes(s, ignore=None, bias=None):
     for k, v in consts.BIG_PREFIXES.items():
         if k.lower() in ignore:
             continue
-        pat = re.compile(r'(?:<num>)?(\d*) *{}'.format(k.lower()), flags=re.IGNORECASE)
+        pat = re.compile(r'(?:<num>)?(\d*\.?\d*) *{}'.format(k.lower()), flags=re.IGNORECASE)
         m = re.search(pat, s)
         if m is not None:
             def _repl_big_prefixes(m):
                 try:
                     if m.group(1):
-                        repl = '<num>' + str(v * int(m.group(1)))
+                        try:
+                            group_1_numberized = int(m.group(1))
+                        except ValueError:
+                            group_1_numberized = float(m.group(1))
+                        repl = '<num>' + str(int(v * group_1_numberized))
                     else:
                         repl = str(v)
                 except IndexError:
